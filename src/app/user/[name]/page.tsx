dadc2a50d9b1d11a15403574ff9d2de4f15e5fc4
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2024-04-12 00:21:01 +0900

    add dynamic(partially static) page

---
+++ src/app/user/[name]/page.tsx
@@ -0,0 +1,24 @@
+import { notFound } from "next/navigation";
+import { FC } from "react";
+
+export const generateStaticParams = () => {
+  return [{ name: "vercel" }, { name: "y-hiraoka" }];
+};
+
+const Page: FC<{
+  params: { name: string };
+}> = async ({ params }) => {
+  const userResponse = await fetch(
+    `https://api.github.com/users/${params.name}`
+  );
+
+  if (userResponse.status === 404) {
+    return notFound();
+  }
+
+  const user = await userResponse.json();
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>;
+};
+
+export default Page;
